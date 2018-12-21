333177a68ebc12ec716af1049143f4c524b0cd7b
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-21 11:21:51 -0500

    Export `plotPCACovariates()`

---
+++ R/plotPCACovariates.R
@@ -0,0 +1,12 @@
+#' Find correlation between principal components (PCs) and covariates
+#' 
+#' @export
+#' @inheritParams params
+#'
+#' @return `ggplot`.
+setGeneric(
+    name = "plotPCACovariates",
+    def = function(object, ...) {
+        standardGeneric("plotPCACovariates")
+    }
+)
