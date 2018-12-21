f1de19edee675711f73dd04745c757cf7c59e455
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-21 13:20:08 -0500

    `contrastSamples()`: Initial commit

---
+++ R/contrastSamples.R
@@ -0,0 +1,9 @@
+#' Samples corresponding to a differential expression contrast
+#' @export
+#' @inheritParams params
+setGeneric(
+    name = "contrastSamples",
+    def = function(object, ...) {
+        standardGeneric("contrastSamples")
+    }
+)
