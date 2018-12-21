bf0b252967fd674476f4537f1875d005cd396591
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-21 17:17:18 -0500

    Improve `plotDEG*` documentation

--- R/plotDEGHeatmap.R
+++ R/plotDEGHeatmap.R
@@ -1,9 +1,9 @@
 #' Differentially expressed gene heatmap
 #'
-#' This function is an extension of `plotHeatmap` that is optimized
-#' for automatic handling differentially expressed genes, rather than requiring
+#' This function is an extension of [plotHeatmap()] that is optimized for
+#' automatic handling differentially expressed genes, rather than requiring
 #' manual input of a gene vector. All of the optional parameters for
-#' `plotHeatmap` are also supported by this function.
+#' [plotHeatmap()] are also supported by this function.
 #'
 #' To adjust the annotation columns, modify the
 #' [colData][SummarizedExperiment::colData] of the `counts` argument, which must
