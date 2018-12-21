cd014c3ee34147dc07a9d446ba0348adb3231958
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-21 13:20:19 -0500

    `resultsTables()`: Initial commit

---
+++ R/resultsTables.R
@@ -0,0 +1,9 @@
+#' Results tables
+#' @export
+#' @inheritParams params
+setGeneric(
+    name = "resultsTables",
+    def = function(object, ...) {
+        standardGeneric("resultsTables")
+    }
+)
