2de8c251ed035a452c89510218448ef49af95995
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-21 17:37:23 -0500

    Finish importing pointillism verbs

---
+++ R/topMarkers.R
@@ -0,0 +1,12 @@
+#' Top markers
+#' 
+#' @rdname topMarkers
+#' @export
+#' 
+#' @return `grouped_df`.
+setGeneric(
+    name = "topMarkers",
+    def = function(object, ...) {
+        standardGeneric("topMarkers")
+    }
+)
