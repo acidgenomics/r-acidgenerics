85676171625262d50c261ee487c9ebb9622d3779
Michael Steinbaugh <mike@steinbaugh.com>
2018-12-21 17:37:23 -0500

    Finish importing pointillism verbs

---
+++ R/plotViolin.R
@@ -0,0 +1,12 @@
+#' Violin plot
+#' 
+#' @export
+#' @inheritParams params
+#' 
+#' @return `ggplot`.
+setGeneric(
+    name = "plotViolin",
+    def = function(object, ...) {
+        standardGeneric("plotViolin")
+    }
+)
