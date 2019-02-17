f64e022519209b7dc089ed817ab22a4aaaae858f
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-17 15:26:48 -0500

    Revert back to formal deprecation

--- R/deprecated.R
+++ R/deprecated.R
@@ -28,17 +28,13 @@ NULL
 
 
 # v0.1.7 =======================================================================
-# Formally deprecate this once we migrate bcbio R packages to v0.3 series.
 #' @rdname deprecated
 #' @export
 setGeneric(
     name = "plotGene",
     def = function(object, ...) {
-        message(paste(
-            "'plotGene' is soft deprecated.",
-            "Use 'plotCounts' instead.",
-            sep = "\n"
-        ))
+        .Deprecated("plotCounts")
+        standardGeneric("plotGene")
     }
 )
 
