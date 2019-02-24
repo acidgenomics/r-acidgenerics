5e1cb263c02e59cc462a63b0e92ce1597c726771
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-24 10:14:57 -0500

    Add `plotIntergenicMappingRate()`

---
+++ R/plotIntergenicMappingRate.R
@@ -0,0 +1,12 @@
+#' Plot intergenic mapping rate
+#'
+#' @export
+#' @inheritParams params
+#'
+#' @return `ggplot`.
+setGeneric(
+    name = "plotIntergenicMappingRate",
+    def = function(object, ...) {
+        standardGeneric("plotIntergenicMappingRate")
+    }
+)
