3696b50b56547103bcc3a588a13a19a5ae89d470
Michael Steinbaugh <mike@steinbaugh.com>
2019-02-25 08:28:13 -0500

    Add kebab case and rebuild website

--- R/snake.R
+++ R/snake.R
@@ -1,4 +1,10 @@
 #' Snake case
+#' 
+#' Format character strings to use snake-style formatting, where word boundaries
+#' are defined by underscores (e.g. `this_is_snake_case`).
+#' 
+#' Snake case is recommended by the tidyverse for variable and function names.
+#' 
 #' @export
 #' @inheritParams params
 setGeneric(
