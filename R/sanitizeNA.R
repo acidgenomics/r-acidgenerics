131f2175d12be0ffb9077e4ad23ad5164a010821
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-23 11:01:10 -0400

    Documentation fixes

--- R/sanitizeNA.R
+++ R/sanitizeNA.R
@@ -1,4 +1,4 @@
-#' Sanitize `NA` values
+#' Sanitize NA values
 #'
 #' Standardize empty strings (`""`), character NAs (`"NA"`), and `NULL` values
 #' inside a character vector to `NA_character_`. Other `atomic` data types are
