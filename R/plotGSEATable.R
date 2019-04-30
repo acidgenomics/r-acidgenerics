51167379cdc3f4e85c9431bb8d8f9b8d7d167668
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-30 12:19:47 -0400

    Add additional S4 generics from pfgsea

---
+++ R/plotGSEATable.R
@@ -0,0 +1,9 @@
+#' Plot GSEA enrichment table
+#' @inheritParams params
+#' @export
+setGeneric(
+    name = "plotGSEATable",
+    def = function(object, ...) {
+        standardGeneric("plotGSEATable")
+    }
+)
