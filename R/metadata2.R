c14ce2607d755fb17ee60a4063bbe476bc36d311
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-18 08:40:57 -0500

    Prepare release, renamed as acidgenerics

--- R/metadata2.R
+++ R/metadata2.R
@@ -1,9 +1,32 @@
 #' Metadata
 #'
-#' @export
-#' @inheritParams acidroxygen::params
+#' Dynamically handles metadata assignment and extraction in a similar fashion
+#' for both S3 and S4 objects.
 #'
-#' @return `list`.
+#' @section S3 class:
+#'
+#' Internally slots into [`attributes()`][base::attributes].
+#' Attribute is also accessible via `attr(object, which)`.
+#'
+#' @section S4 class:
+#'
+#' Requires that object extends [`Annotated`][S4Vectors::Annotated-class] class,
+#' which supports [`metadata()`][S4Vectors::metadata].
+#'
+#' Internally slots into [`metadata()`][S4Vectors::metadata].
+#' Attribute is also accessible via `metadata(object)[[which]]`.
+#'
+#' @inheritParams acidroxygen::params
+#' @param which `character(1)`.
+#'   A non-empty character string specifying which attribute is to be accessed.
+#'   Note that positional `numeric` arguments are currently not allowed.
+#' @param value `ANY`.
+#'   Metadata values to assign into slot, defined by `which`.
+#'
+#' @return
+#' - `metadata2()`: `list`. Returns `NULL` on `which` match failure, similar
+#'   to [`attr()`][base::attr] and [`metadata()`][S4Vectors::metadata].
+#' - `metadata2<-()`: Modified object.
 #'
 #' @examples
 #' class(metadata2)
