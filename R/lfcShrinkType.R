e26b9d136ea9648019f6190998121f3ef182682b
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-04 12:46:28 -0400

    Add lfcShrinkType

---
+++ R/lfcShrinkType.R
@@ -0,0 +1,16 @@
+#' Shrunken log2 fold change (LFC) type
+#'
+#' @export
+#' @inheritParams acidroxygen::params
+#'
+#' @return `character(1)` or `NULL`.
+#'   Name of adaptive shrinkage method applied (e.g. "apeglm" or "ashr").
+#'
+#' @examples
+#' class(lfcShrinkType)
+setGeneric(
+    name = "lfcShrinkType",
+    def = function(object, ...) {
+        standardGeneric("lfcShrinkType")
+    }
+)
