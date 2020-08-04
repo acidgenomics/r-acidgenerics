a8eb07e2b94e5f753b8c759052f80ebf616cb07b
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-04 12:39:42 -0400

    Export baseMeanThreshold

---
+++ R/baseMeanThreshold.R
@@ -0,0 +1,27 @@
+#' Base mean threshold
+#'
+#' @export
+#' @inheritParams acidroxygen::params
+#'
+#' @return `numeric(1)`.
+#' Base mean cutoff level.
+#'
+#' @examples
+#' class(baseMeanThreshold)
+setGeneric(
+    name = "baseMeanThreshold",
+    def = function(object, ...) {
+        standardGeneric("baseMeanThreshold")
+    }
+)
+
+
+
+#' @rdname baseMeanThreshold
+#' @export
+setGeneric(
+    name = "baseMeanThreshold<-",
+    def = function(object, ..., value) {
+        standardGeneric("baseMeanThreshold<-")
+    }
+)
