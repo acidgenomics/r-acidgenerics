4b67efcdfa912534e24e6ba3ce7349056359fb75
Michael Steinbaugh <mike@steinbaugh.com>
2020-08-04 12:49:54 -0400

    Add plotBaseMean

---
+++ R/plotBaseMean.R
@@ -0,0 +1,18 @@
+#' Plot base mean distribution
+#'
+#' The base mean is the mean of normalized counts of all samples, normalizing
+#' for sequencing depth.
+#'
+#' @export
+#' @inheritParams acidroxygen::params
+#'
+#' @return `ggplot`.
+#'
+#' @examples
+#' class(plotBaseMean)
+setGeneric(
+    name = "plotBaseMean",
+    def = function(object, ...) {
+        standardGeneric("plotBaseMean")
+    }
+)
