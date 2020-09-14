306bede628d067f00f053dda31a0694d3c7ddda4
Michael Steinbaugh <mike@steinbaugh.com>
2020-09-14 15:20:44 -0400

    Initial commit

---
+++ R/splitByLevel.R
@@ -0,0 +1,27 @@
+#' Split by factor level
+#'
+#' @export
+#' @note Updated 2020-09-14.
+#'
+#' @param f `character(1)`.
+#'   Factor column name.
+#' @param ref `logical(1)`.
+#'   Include the reference factor level (i.e. first defined) in each split.
+#'   Useful for pairwise contrasts.
+#'
+#' @return `list`
+#' List of data frames split by factors.
+#'
+#' @seealso
+#' - `split()`
+#' - `S4Vectors::split()`
+#' - `dplyr::group_split()`
+#'
+#' @examples
+#' class(splitByLevel)
+setGeneric(
+    name = "splitByLevel",
+    def = function(x, f, ref = FALSE, ...) {
+        standardGeneric("splitByLevel")
+    }
+)
