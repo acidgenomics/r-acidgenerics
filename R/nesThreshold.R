4098494043eaa7afe521223886940e7237c7143e
Michael Steinbaugh <mike@steinbaugh.com>
2020-09-21 08:50:43 -0400

    Add generics

---
+++ R/nesThreshold.R
@@ -0,0 +1,26 @@
+#' NES threshold
+#'
+#' @export
+#' @inheritParams acidroxygen::params
+#'
+#' @return `numeric(1)`.
+#'
+#' @examples
+#' class(nesThreshold)
+setGeneric(
+    name = "nesThreshold",
+    def = function(object, ...) {
+        standardGeneric("nesThreshold")
+    }
+)
+
+
+
+#' @rdname nesThreshold
+#' @export
+setGeneric(
+    name = "nesThreshold<-",
+    def = function(object, ..., value) {
+        standardGeneric("nesThreshold<-")
+    }
+)
