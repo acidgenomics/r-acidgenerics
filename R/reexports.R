1d541b32272d98b8c482c6df4ecea2d3c7420214
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-15 15:53:46 -0500

    Draft update to reexports

--- R/reexports.R
+++ R/reexports.R
@@ -1,19 +1,33 @@
-# counts
-# counts<-
-# estimateSizeFactors
-# normalize
-# plotDispEsts
 # rowMeans
 # rowSums
 # sizeFactors
 # sizeFactors<-
-# as.data.frame
 
 
+
+#' @importFrom BiocGenerics as.data.frame
+#' @export
+BiocGenerics::as.data.frame
 
 #' @importFrom BiocGenerics combine
 #' @export
 BiocGenerics::combine
+
+#' @importFrom BiocGenerics counts
+#' @export
+BiocGenerics::counts
+
+#' @importFrom BiocGenerics counts<-
+#' @export
+BiocGenerics::`counts<-`
+
+#' @importFrom BiocGenerics estimateSizeFactors
+#' @export
+BiocGenerics::estimateSizeFactors
+
+#' @importFrom BiocGenerics normalize
+#' @export
+BiocGenerics::normalize
 
 #' @importFrom BiocGenerics organism
 #' @export
@@ -22,6 +36,10 @@ BiocGenerics::organism
 #' @importFrom BiocGenerics organism<-
 #' @export
 BiocGenerics::`organism<-`
+
+#' @importFrom BiocGenerics plotDispEsts
+#' @export
+BiocGenerics::plotDispEsts
 
 #' @importFrom BiocGenerics plotMA
 #' @export
@@ -45,12 +63,28 @@ BiocGenerics::updateObject
 
 
 
+#' @importFrom S4Vectors aggregate
+#' @export
+S4Vectors::aggregate
+
 #' @importFrom S4Vectors decode
 #' @export
 S4Vectors::decode
+
+#' @importFrom S4Vectors head
+#' @export
+S4Vectors::head
+
+#' @importFrom S4Vectors merge
+#' @export
+S4Vectors::merge
 
 #' @rdname reexports
 #' @name summary
 #' @importFrom S4Vectors summary
 #' @export
 NULL
+
+#' @importFrom S4Vectors tail
+#' @export
+S4Vectors::tail
