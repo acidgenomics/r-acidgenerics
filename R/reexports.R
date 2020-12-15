e903a8aa5007c4e264f1c49c7281eca2a6e3e06a
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-15 15:46:15 -0500

    Draft update to generic reexports

--- R/reexports.R
+++ R/reexports.R
@@ -1,3 +1,56 @@
+# counts
+# counts<-
+# estimateSizeFactors
+# normalize
+# plotDispEsts
+# rowMeans
+# rowSums
+# sizeFactors
+# sizeFactors<-
+# as.data.frame
+
+
+
+#' @importFrom BiocGenerics combine
+#' @export
+BiocGenerics::combine
+
+#' @importFrom BiocGenerics organism
+#' @export
+BiocGenerics::organism
+
+#' @importFrom BiocGenerics organism<-
+#' @export
+BiocGenerics::`organism<-`
+
+#' @importFrom BiocGenerics plotMA
+#' @export
+BiocGenerics::plotMA
+
+#' @importFrom BiocGenerics plotPCA
+#' @export
+BiocGenerics::plotPCA
+
+#' @importFrom BiocGenerics table
+#' @export
+BiocGenerics::table
+
+#' @importFrom BiocGenerics unlist
+#' @export
+BiocGenerics::unlist
+
+#' @importFrom BiocGenerics updateObject
+#' @export
+BiocGenerics::updateObject
+
+
+
 #' @importFrom S4Vectors decode
 #' @export
 S4Vectors::decode
+
+#' @rdname reexports
+#' @name summary
+#' @importFrom S4Vectors summary
+#' @export
+NULL
