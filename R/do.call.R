d2b51df049af0c52500015262f48052c07ae7b78
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-21 16:46:51 -0500

    Finish documenting "d" functions

--- R/do.call.R
+++ R/do.call.R
@@ -1,13 +1,22 @@
 #' Execute a function call
 #'
 #' @name do.call
-#' @note Updated 2020-12-21.
-#' @export
-#'
 #' @importFrom BiocGenerics do.call
+#' @export
 #'
 #' @usage do.call(what, args, quote = FALSE, envir = parent.frame())
 #'
 #' @param what,args,quote,envir
 #'   See [`base::do.call`][base::do.call] for details.
+#'
+#' @return Varies, depending on function called.
+#'
+#' @examples
+#' showMethods("do.call")
+NULL
+
+
+
+#' @importMethodsFrom S4Vectors do.call
+#' @exportMethod do.call
 NULL
