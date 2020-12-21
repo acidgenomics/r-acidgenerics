6945a0e2300799223cdf07ef0cefb65ccf721fad
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-21 15:13:39 -0500

    Split out to separate documentation file

---
+++ R/counts.R
@@ -0,0 +1,21 @@
+#' Counts
+#'
+#' @name counts
+#' @note Updated 2020-12-21.
+#'
+#' @param object Object.
+#' @param value Value to assign.
+#' @param ... Additional arguments.
+NULL
+
+#' @rdname counts
+#' @name counts
+#' @importFrom BiocGenerics counts
+#' @export
+NULL
+
+#' @rdname counts
+#' @name counts<-
+#' @importFrom BiocGenerics counts<-
+#' @export
+NULL
