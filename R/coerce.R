2f848a061f6252b534a50ac3ebcce76fe2a389d0
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-21 16:25:37 -0500

    Improve documentation

---
+++ R/coerce.R
@@ -0,0 +1,14 @@
+#' Coerce an object to a class
+#'
+#' @name coerce
+#' @importFrom S4Vectors coerce
+#' @export
+#'
+#' @usage coerce(from, to, strict = TRUE)
+#'
+#' @param from,to,strict
+#'   See [`methods::coerce`][methods::coerce] for details.
+#'
+#' @examples
+#' showMethods("coerce")
+NULL
