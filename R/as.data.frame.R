3e8213ddf8ce7d30d696921d961999efedf51425
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-21 14:02:18 -0500

    Improve documentation

---
+++ R/as.data.frame.R
@@ -0,0 +1,12 @@
+#' Coerce object to a data frame
+
+#' @name as.data.frame
+#'
+#' @export
+#' @importFrom BiocGenerics as.data.frame
+#'
+#' @usage as.data.frame(x, row.names = NULL, optional = FALSE, ...)
+#'
+#' @param x,row.names,optional,...
+#'   See [base::as.data.frame()] for details.
+NULL
