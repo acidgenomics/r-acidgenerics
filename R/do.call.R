530634fb08494b5e36d5c3405d0378446a014d9c
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-21 15:17:58 -0500

    Split out to separate file

---
+++ R/do.call.R
@@ -0,0 +1,13 @@
+#' Execute a function call
+#'
+#' @name do.call
+#' @note Updated 2020-12-21.
+#' @export
+#'
+#' @importFrom BiocGenerics do.call
+#'
+#' @usage do.call(what, args, quote = FALSE, envir = parent.frame())
+#'
+#' @param what,args,quote,envir
+#'   See [`base::do.call`][base::do.call] for details.
+NULL
