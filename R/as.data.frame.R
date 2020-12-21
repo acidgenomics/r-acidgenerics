ee31cbdb6ed40664d6f6cad7d7e35fd7ec38ec67
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-21 14:12:24 -0500

    Documentation fixes

--- R/as.data.frame.R
+++ R/as.data.frame.R
@@ -1,8 +1,9 @@
 #' Coerce object to a data frame
 
 #' @name as.data.frame
-#'
+#' @note Updated 2020-12-21.
 #' @export
+#'
 #' @importFrom BiocGenerics as.data.frame
 #'
 #' @usage as.data.frame(x, row.names = NULL, optional = FALSE, ...)
