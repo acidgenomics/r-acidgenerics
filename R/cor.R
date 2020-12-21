8a5a6653e88737e62e9f9680c817a03ef4d452a4
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-21 16:16:25 -0500

    Improve documentation

---
+++ R/cor.R
@@ -0,0 +1,20 @@
+#' Correlation
+#'
+#' @name cor
+#' @importFrom S4Vectors cor
+#' @export
+#'
+#' @usage
+#' cor(
+#'     x,
+#'     y = NULL,
+#'     use = "everything",
+#'     method = c("pearson", "kendall", "spearman")
+#' )
+#'
+#' @param x,y,use,method
+#'   See [`stats::cor`][stats::cor] for details.
+#'
+#' @examples
+#' class(cor)
+NULL
