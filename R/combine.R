dbf7477500af9277b41bf7adc0d21ab7ee404f06
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-21 14:12:24 -0500

    Documentation fixes

---
+++ R/combine.R
@@ -0,0 +1,14 @@
+#' Combine multiple objects
+#'
+#' @name combine
+#' @note Updated 2020-12-21.
+#' @export
+#'
+#' @importFrom BiocGenerics combine
+#'
+#' @usage combine(x, y, ...)
+#'
+#' @param x Primary object.
+#' @param y Secondary object.
+#' @param ... Additional arguments.
+NULL
