1236a8e2456edcea171578c9865b096fe33c9585
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-21 15:20:22 -0500

    Split out to separate file

---
+++ R/estimateSizeFactors.R
@@ -0,0 +1,13 @@
+#' Estimate size factors
+#'
+#' @name estimateSizeFactors
+#' @note Updated 2020-12-21.
+#' @export
+#'
+#' @importFrom BiocGenerics estimateSizeFactors
+#'
+#' @usage estimateSizeFactors(object, ...)
+#'
+#' @param object Object.
+#' @param ... Additional arguments.
+NULL
