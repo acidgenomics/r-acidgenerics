7c2aa49836a60950cd3c38bdb248732eeea1a34a
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-21 14:10:04 -0500

    Improve documentation

---
+++ R/colSums.R
@@ -0,0 +1,11 @@
+#' Column sums
+#'
+#' @name colSums
+#' @note Updated 2020-12-21.
+#' @export
+#'
+#' @importFrom BiocGenerics colSums
+#' @usage colSums(x, na.rm = FALSE, dims = 1, ...)
+#' @param x,na.rm,dims,...
+#'   See [`base::colSums`][base::colSums] for details.
+NULL
