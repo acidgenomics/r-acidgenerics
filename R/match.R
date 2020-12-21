980f67373771212187b77c3e21a17da034dbf80d
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-21 13:55:29 -0500

    Split out reexports

---
+++ R/match.R
@@ -0,0 +1,25 @@
+#' Value matching
+#'
+#' @name match
+#' @note Updated 2020-12-21.
+#'
+#' @param x,table
+#'   Vector-like objects (typically of the same class, but not necessarily).
+#' @param nomatch,incomparables
+#'   See [base::match][] for a description of these arguments.
+#' @param ... Additional arguments.
+NULL
+
+#' @rdname match
+#' @name match
+#' @importFrom BiocGenerics match
+#' @usage match(x, table, nomatch = NA_integer_, incomparables = NULL, ...)
+#' @export
+NULL
+
+#' @rdname match
+#' @name %in%
+#' @importFrom BiocGenerics %in%
+#' @usage x %in% table
+#' @export
+NULL
---
+++ R/sampleNames.R
@@ -0,0 +1,23 @@
+#' Sample names
+#'
+#' @name sampleNames
+#' @note Updated 2020-12-21.
+#'
+#' @param object Object.
+#' @param value Value to assign.
+#' @param ... Additional arguments.
+NULL
+
+#' @rdname sampleNames
+#' @name sampleNames
+#' @importFrom Biobase sampleNames
+#' @usage sampleNames(object)
+#' @export
+NULL
+
+#' @rdname sampleNames
+#' @name sampleNames<-
+#' @importFrom Biobase sampleNames<-
+#' @usage sampleNames(object) <- value
+#' @export
+NULL
