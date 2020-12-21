d97fb36f089cb4fda911de3ac1425809a420826b
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-21 14:06:23 -0500

    Improve documentation

---
+++ R/as.list.R
@@ -0,0 +1,13 @@
+#' Coerce object to a list
+#'
+#' @name as.list
+#' @note Updated 2020-12-12.
+#' @export
+#'
+#' @importFrom BiocGenerics as.list
+#'
+#' @usage as.list(x, ...)
+#'
+#' @param x Object.
+#' @param ... Additional arguments.
+NULL
