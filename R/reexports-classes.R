23c7a0d2a80bc4bc283b2bf5e68316e56b351606
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-21 09:26:01 -0500

    Reexport AsIs definition from BiocGenerics

---
+++ R/reexports-classes.R
@@ -0,0 +1,14 @@
+#' Reexport S4 classes
+#'
+#' @name reexports-classes
+#' @note Updated 2020-12-21.
+#' @keywords internal
+NULL
+
+
+
+#' @rdname reexports-classes
+#' @name Asis
+#' @importClassesFrom BiocGenerics AsIs
+#' @exportClass AsIs
+NULL
