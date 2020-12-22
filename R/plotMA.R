23f660a9b131dd3bfe936053d88c1427964f390c
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-22 09:39:52 -0500

    Improve documentation

--- R/plotMA.R
+++ R/plotMA.R
@@ -1,5 +1,20 @@
-#' @rdname reexports
+#' MA plot
+#'
+#' The plot visualizes the differences between measurements taken in two
+#' samples, by transforming the data onto **M** (log ratio) and **A**
+#' (mean average) scales.
+#'
 #' @name plotMA
 #' @importFrom BiocGenerics plotMA
 #' @export
+#'
+#' @usage plotMA(object, ...)
+#'
+#' @param object Object.
+#' @param ... Additional arguments.
+#'
+#' @return `ggplot`.
+#'
+#' @examples
+#' showMethods("plotMA')
 NULL
