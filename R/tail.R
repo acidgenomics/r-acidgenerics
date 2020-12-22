a2b8b5460db0d3c17069857747d68d5926689285
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-22 09:41:38 -0500

    Improve documentation

---
+++ R/tail.R
@@ -0,0 +1,16 @@
+#' Return the last part of an object
+#'
+#' @name tail
+#' @importFrom S4Vectors tail
+#' @export
+#'
+#' @usage tail(x, ...)
+#'
+#' @param x Object.
+#' @param ... Additional arguments.
+#'
+#' @return Console output.
+#'
+#' @examples
+#' showMethods("tail")
+NULL
