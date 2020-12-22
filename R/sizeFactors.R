3e45d99115a9f19862f8117b0d8d6749869c9e19
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-22 09:46:03 -0500

    Improve documentation

--- R/sizeFactors.R
+++ R/sizeFactors.R
@@ -1,11 +1,29 @@
-#' @rdname reexports
+#' Size factors
+#'
+#' @name sizeFactors
+#'
+#' @param object Object.
+#' @param value Value to assign.
+#' @param ... Additional arguments.
+#'
+#' @return Varies, depending on the method.
+#'
+#' @examples
+#' showMethods("sizeFactors")
+NULL
+
+
+
+#' @rdname sizeFactors
 #' @name sizeFactors
 #' @importFrom BiocGenerics sizeFactors
+#' @usage sizeFactors(object, ...)
 #' @export
 NULL
 
-#' @rdname reexports
+#' @rdname sizeFactors
 #' @name sizeFactors<-
 #' @importFrom BiocGenerics sizeFactors<-
+#' @usage sizeFactors(object, ...) <- value
 #' @export
 NULL
