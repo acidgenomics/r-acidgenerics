8242d0112f89d8800b28e59f4d6f5d3ab824627b
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-22 09:39:52 -0500

    Improve documentation

--- R/plotPCA.R
+++ R/plotPCA.R
@@ -1,5 +1,16 @@
-#' @rdname reexports
+#' Principal component analysis plot
+#'
 #' @name plotPCA
 #' @importFrom BiocGenerics plotPCA
 #' @export
+#'
+#' @usage plotPCA(object, ...)
+#'
+#' @param object Object.
+#' @param ... Additional arguments.
+#'
+#' @return `ggplot`.
+#'
+#' @examples
+#' showMethods("plotPCA")
 NULL
