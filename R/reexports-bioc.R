3551506e70b2eaf8b0f43f66c423386c3ebcd0a2
Michael Steinbaugh <mike@steinbaugh.com>
2020-12-22 09:40:47 -0500

    Consolidate some Bioconductor generics that don't need to inherit documentation in other packages

---
+++ R/reexports-bioc.R
@@ -0,0 +1,234 @@
+#' Bioconductor reexports
+#'
+#' @name reexports-bioc
+#' @docType import
+#' @keywords internal
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name AsIs-class
+#' @importClassesFrom BiocGenerics AsIs
+#' @exportClass AsIs
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name %in%
+#' @importFrom BiocGenerics %in%
+#' @export
+NULL
+
+#' @importMethodsFrom S4Vectors %in%
+#' @exportMethod %in%
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name colSums
+#' @importFrom BiocGenerics colSums
+#' @export
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name complete.cases
+#' @importFrom S4Vectors complete.cases
+#' @export
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name cor
+#' @importFrom S4Vectors cor
+#' @export
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name do.call
+#' @importFrom BiocGenerics do.call
+#' @export
+NULL
+
+#' @importMethodsFrom S4Vectors do.call
+#' @exportMethod do.call
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name expand
+#' @importFrom S4Vectors expand
+#' @export
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name expand.grid
+#' @importFrom S4Vectors expand.grid
+#' @export
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name lapply
+#' @importFrom BiocGenerics lapply
+#' @export
+NULL
+
+#' @importMethodsFrom S4Vectors lapply
+#' @exportMethod lapply
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name match
+#' @importFrom BiocGenerics match
+#' @export
+NULL
+
+#' @importMethodsFrom S4Vectors match
+#' @exportMethod match
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name mcols
+#' @importFrom S4Vectors mcols
+#' @export
+NULL
+
+#' @rdname reexports-bioc
+#' @name mcols<-
+#' @importFrom S4Vectors mcols<-
+#' @export
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name merge
+#' @importFrom S4Vectors merge
+#' @export
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name metadata
+#' @importFrom S4Vectors metadata
+#' @export
+NULL
+
+#' @rdname reexports-bioc
+#' @name metadata<-
+#' @importFrom S4Vectors metadata<-
+#' @export
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name na.omit
+#' @importFrom S4Vectors na.omit
+#' @export
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name order
+#' @importFrom BiocGenerics order
+#' @export
+NULL
+
+#' @importMethodsFrom S4Vectors order
+#' @exportMethod order
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name rowMeans
+#' @importFrom BiocGenerics rowMeans
+#' @export
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name rowSums
+#' @importFrom BiocGenerics rowSums
+#' @export
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name setdiff
+#' @importFrom BiocGenerics setdiff
+#' @export
+NULL
+
+#' @importMethodsFrom S4Vectors setdiff
+#' @exportMethod setdiff
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name split
+#' @importFrom S4Vectors split
+#' @export
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name t
+#' @importFrom BiocGenerics t
+#' @export
+NULL
+
+#' @importMethodsFrom S4Vectors t
+#' @exportMethod t
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name table
+#' @importFrom BiocGenerics table
+#' @export
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name unique
+#' @importFrom BiocGenerics unique
+#' @export
+NULL
+
+#' @importMethodsFrom S4Vectors unique
+#' @exportMethod unique
+NULL
+
+
+
+#' @rdname reexports-bioc
+#' @name unlist
+#' @importFrom BiocGenerics unlist
+#' @export
+NULL
