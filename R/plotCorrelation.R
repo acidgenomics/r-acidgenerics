985a963d02da0106d8bfa1978a7d97f82464caae
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-09 11:22:07 -0500

    Add `plotCorrelation` generic

---
+++ R/plotCorrelation.R
@@ -0,0 +1,15 @@
+#' Correlation X-Y scatterplot
+#'
+#' @export
+#' @inheritParams AcidRoxygen::params
+#'
+#' @return Plot.
+#'
+#' @examples
+#' showMethods("plotCorrelation")
+setGeneric(
+    name = "plotCorrelation",
+    def = function(object, ...) {
+        standardGeneric("plotCorrelation")
+    }
+)
