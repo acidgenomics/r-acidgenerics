d80db4d85f56a8c33171f5082e09db59ffc9607f
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-12 15:00:43 -0500

    Add functions from IRanges

--- R/bioc-reexports-01-functions.R
+++ R/bioc-reexports-01-functions.R
@@ -1,10 +1,30 @@
+#' @importFrom IRanges CharacterList
+#' @export
+IRanges::CharacterList
+
 #' @importFrom S4Vectors DataFrame
 #' @export
 S4Vectors::DataFrame
 
+#' @importFrom IRanges DataFrameList
+#' @export
+IRanges::DataFrameList
+
 #' @importFrom S4Vectors Factor
 #' @export
 S4Vectors::Factor
+
+#' @importFrom IRanges FactorList
+#' @export
+IRanges::FactorList
+
+#' @importFrom IRanges IRanges
+#' @export
+IRanges::IRanges
+
+#' @importFrom IRanges IntegerList
+#' @export
+IRanges::IntegerList
 
 #' @importFrom S4Vectors LLint
 #' @export
@@ -14,10 +34,26 @@ S4Vectors::LLint
 #' @export
 S4Vectors::List
 
+#' @importFrom IRanges LogicalList
+#' @export
+IRanges::LogicalList
+
+#' @importFrom IRanges NumericList
+#' @export
+IRanges::NumericList
+
 #' @importFrom S4Vectors Rle
 #' @export
 S4Vectors::Rle
 
+#' @importFrom IRanges RleList
+#' @export
+IRanges::RleList
+
 #' @importFrom S4Vectors SimpleList
 #' @export
 S4Vectors::SimpleList
+
+#' @importFrom IRanges SplitDataFrameList
+#' @export
+IRanges::SplitDataFrameList
