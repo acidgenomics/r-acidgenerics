5f6680b0906161b1a6ba31f3267de14b850e8bc1
Michael Steinbaugh <mike@steinbaugh.com>
2021-09-28 13:21:21 -0400

    Include `as.DFrame` alias

--- R/as.DataFrame.R
+++ R/as.DataFrame.R
@@ -1,12 +1,29 @@
 #' Coerce object to S4 DataFrame
 #'
-#' @export
+#' @name as.DataFrame
 #' @inheritParams AcidRoxygen::params
 #'
 #' @return `DataFrame`.
 #'
 #' @examples
 #' showMethods("as.DataFrame")
+NULL
+
+
+
+#' @rdname as.DataFrame
+#' @export
+setGeneric(
+    name = "as.DFrame",
+    def = function(x, ...) {
+        standardGeneric("as.DFrame")
+    }
+)
+
+
+
+#' @rdname as.DataFrame
+#' @export
 setGeneric(
     name = "as.DataFrame",
     def = function(x, ...) {
