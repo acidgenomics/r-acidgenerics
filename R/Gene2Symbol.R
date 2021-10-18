e9d7370fb15654558240a1e62cd59e07218dd2c5
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-18 09:27:58 -0400

    Migrate generics from AcidGenomes

---
+++ R/Gene2Symbol.R
@@ -0,0 +1,15 @@
+#' Gene-to-symbol mappings
+#'
+#' @export
+#' @inheritParams AcidRoxygen::params
+#'
+#' @return `Gene2Symbol`.
+#'
+#' @examples
+#' showMethods("Gene2Symbol")
+setGeneric(
+    name = "Gene2Symbol",
+    def = function(object, ...) {
+        standardGeneric("Gene2Symbol")
+    }
+)
