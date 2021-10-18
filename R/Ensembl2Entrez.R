2db3613591f7ee542f2ac78c291d85fad71144af
Michael Steinbaugh <mike@steinbaugh.com>
2021-10-18 09:25:24 -0400

    Migrate these generics from AcidGenomes

---
+++ R/Ensembl2Entrez.R
@@ -0,0 +1,12 @@
+#' Ensembl-to-Entrez gene identifier mappings
+#'
+#' @return `Ensembl2Entrez`.
+#'
+#' @examples
+#' showMethods("Ensembl2Entrez")
+setGeneric(
+    name = "Ensembl2Entrez",
+    def = function(object, ...) {
+        standardGeneric("Ensembl2Entrez")
+    }
+)
