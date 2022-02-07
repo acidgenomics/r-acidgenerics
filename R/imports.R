7023a705b358fb7c876a15e0fbf900ca5ced685b
Michael Steinbaugh <mike@steinbaugh.com>
2022-02-07 13:04:07 -0500

    Reorganize imports

---
+++ R/imports.R
@@ -0,0 +1,2 @@
+#' @importFrom methods setGeneric
+NULL
--- R/package.R
+++ R/package.R
@@ -3,6 +3,4 @@
 #' S4 generic functions for Acid Genomics packages.
 #'
 #' @keywords internal
-#'
-#' @importFrom methods setGeneric
 "_PACKAGE"
