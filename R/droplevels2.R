af8ab42a7ab9125eeb18379c8c56efa058ba2504
Michael Steinbaugh <mike@steinbaugh.com>
2022-04-25 15:11:34 -0400

    Create a new droplevels generic that doesn't collide with Bioc 3.15 update

---
+++ R/droplevels2.R
@@ -0,0 +1,15 @@
+#' Drop factor levels
+#'
+#' @export
+#' @inheritParams AcidRoxygen::params
+#'
+#' @return Modified object.
+#'
+#' @examples
+#' showMethods("droplevels2")
+setGeneric(
+    name = "droplevels2",
+    def = function(x, ...) {
+        standardGeneric("droplevels2")
+    }
+)
