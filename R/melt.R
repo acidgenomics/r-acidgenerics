19cc73793f71a8443a58490e38eb11e7a1e0da1c
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-16 08:52:28 -0400

    Add `cast` generic support

--- R/melt.R
+++ R/melt.R
@@ -1,4 +1,4 @@
-#' Melt columns into key-value pairs
+#' Melt data from wide to long format
 #'
 #' Unpivot column data from wide format to long format.
 #'
@@ -6,8 +6,9 @@
 #' @inheritParams AcidRoxygen::params
 #'
 #' @seealso
-#' - `tidyr::gather()`: Recommended instead for use with `tbl_df`.
-#' - `reshape2::melt()`: Inspiration for name, but now deprecated..
+#' - https://seananderson.ca/2013/10/19/reshape/
+#' - `reshape2::melt` (deprecated).
+#' - `tidyr::pivot_longer`.
 #' - Python `pandas.melt`.
 #'
 #' @return `DataFrame`.
