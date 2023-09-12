52deb67b12773f099842ab2aaf61c8f2e7dac442
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-12 07:58:59 -0400

    Migrate selectCells here from DepMapAnalysis

---
+++ R/selectCells.R
@@ -0,0 +1,22 @@
+#' Select cells
+#'
+#' @export
+#' @inheritParams AcidRoxygen::params
+#'
+#' @param ...
+#' Named key value pairs that map to cell line metadata defined in `colData`.
+#'
+#' @return Modified object.
+#'
+#' @seealso
+#' - `Cellosaurus` package.
+#' - `DepMapAnalysis` package.
+#'
+#' @examples
+#' showMethods("selectCells")
+setGeneric(
+    name = "selectCells",
+    def = function(object, ...) {
+        standardGeneric("selectCells")
+    }
+)
