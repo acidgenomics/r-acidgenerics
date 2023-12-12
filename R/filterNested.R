0d227867b81a7d26e18241c6ed7e1c6472c416bd
Michael Steinbaugh <mike@steinbaugh.com>
2023-12-12 13:27:44 -0500

    Add filterNested generic

---
+++ R/filterNested.R
@@ -0,0 +1,24 @@
+#' Keep rows that match a nested condition
+#'
+#' @export
+#' @inheritParams AcidRoxygen::params
+#'
+#' @details
+#' Intended for partial matching with regular expressions. For exact matches,
+#' refer to `matchNested` function.
+#'
+#' @inheritParams AcidRoxygen::params
+#'
+#' @param ignoreCase `logical(1)`.
+#' Perform case insensitive pattern matching.
+#'
+#' @return Modified object, containing only matching rows.
+#'
+#' @examples
+#' showMethods("filterNested")
+setGeneric(
+    name = "filterNested",
+    def = function(object, pattern, ignoreCase, ...) {
+        standardGeneric("filterNested")
+    }
+)
